c6e3ba1cba65f2657e1e112fa91bfc722be01d3b
chlbri <bri_lvi@icloud.com>
2022-12-29 15:48:53 +0000

    minor : Add inner actions Author: chlbri <bri_lvi@icloud.com>

--- src/machines/search/machine.types.ts
+++ src/machines/search/machine.types.ts
@@ -1,12 +1,14 @@
-import { Category } from '~schemas';
+import { FetchNewsQuery } from '~machines/fetchNews/machine.types';
+import { Category, NewsResponse } from '~schemas';
 
 export type Context = {
   currentPage: string;
   input?: string;
   categories: Category[];
+  news?: NewsResponse['news'];
 };
 
 export type Events =
-  | { type: 'INPUT' }
-  | { type: 'SEARCH' }
+  | { type: 'INPUT'; input: string }
+  | ({ type: 'QUERY' } & FetchNewsQuery)
   | { type: 'NAVIGATE'; page: string };
