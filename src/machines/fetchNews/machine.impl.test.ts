1e66b525ef1db334b4cb9733e4c767f19ad0c321
chlbri <bri_lvi@icloud.com>
2022-12-30 00:20:02 +0000

    test

--- src/machines/fetchNews/machine.impl.test.ts
+++ src/machines/fetchNews/machine.impl.test.ts
@@ -1,15 +1,16 @@
 import { interpret } from '@bemedev/x-test';
-import { beforeAll, describe, test } from 'vitest';
+import { beforeAll, describe, test, vi } from 'vitest';
 import { FetchNewsMachine } from './machine';
 
 const { start, matches, send, context, parentSend, stop } =
   interpret(FetchNewsMachine);
 
 beforeAll(() => {
-  global.fetch = (input, init) =>
+  const implementation = (input: any, init: any) =>
     import('node-fetch').then(({ default: fetch }) =>
       fetch(input as any, init as any),
-    ) as any;
+    );
+  global.fetch = vi.fn().mockImplementation(implementation);
 });
 
 describe.skipIf(process.env.GITHUB_ACTIONS !== 'true')(
